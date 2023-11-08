3753970f3579e40059adc4ab553eccff894c75f6
Thomas Williams <noboneslikejelly@gmail.com>
2023-11-08 15:09:50 +1300

    V3,...

--- ExposureMenu.py
+++ ExposureMenu.py
@@ -1,95 +1,98 @@
 import os
-import pygame
 import subprocess
 import threading
+import pygame
 import time
+from pygame.locals import *
 
-# Initialize Pygame
+# Define your video directory and list of video files
+video_directory = "/mnt/usbdrive0/TestExposureRaspi/TestVideos/"
+#video_directory = "./TestExposureRaspi/TestVideos/"
+
+def get_menu_items(video_directory):
+    menu_items = []
+    for filename in os.listdir(video_directory):
+        if filename.lower().endswith(".mp4") or filename.lower().endswith(".mov") and  os.path.isfile(os.path.join(video_directory, filename)):
+            #image_filename = os.path.splitext(filename)[0] + ".png"
+            menu_items.append((filename))
+    return menu_items
+
+video_files = get_menu_items(video_directory)
+
+
+# Pygame initialization
 pygame.init()
+pygame.display.set_caption("Video Player Menu")
 
-# Set screen dimensions
-screen_width = 800
-screen_height = 600
-screen = pygame.display.set_mode((screen_width, screen_height))
+monitor_info = pygame.display.Info()
 
-#folder_path = "./TestExposureRaspi/TestVideos/"
-folder_path = "/mnt/usbdrive0/TestExposureRaspi/TestVideos/"
+SCREEN_WIDTH = monitor_info.current_w
+SCREEN_HEIGHT = monitor_info.current_h
 
-# Global variables
-selected_video = None
-selected_index = 0
-
-# Set font and font size for the menu
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 font = pygame.font.Font(None, 36)
 
-def display_menu(video_list):
-    global selected_video, selected_index
+# Colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+# Initialize VLC subprocess
+vlc_player = None
+
+
+def play_video(video_file):
+    global vlc_player
+    if vlc_player is not None:
+        vlc_player.terminate()
+    #vlc_command = f"sudo -u twilliams /Applications/VLC.app/Contents/MacOS/VLC '{video_file}' --no-repeat --play-and-exit --fullscreen"  # Replace with the appropriate VLC command
+    vlc_command = f"cvlc '{video_file}' --no-repeat --play-and-exit --fullscreen"  # Replace with the appropriate VLC command
+    vlc_player = subprocess.Popen(vlc_command, shell=True)
+    vlc_player.wait()
+
+def input_listener():
+    
+    time.sleep(5)
+    selected_video = os.path.join(video_directory, video_files[selected_index])
+    play_video(selected_video)
+
+
+selected_index = 0
+
+# Main menu loop
+
+if __name__ == "__main__":
+
+
+    input_thread = threading.Thread(target=input_listener)
+    input_thread.daemon = True
+    input_thread.start()
 
     running = True
-
-    
     while running:
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    selected_index = max(0, selected_index - 1)
-                elif event.key == pygame.K_DOWN:
-                    selected_index = min(len(video_list) - 1, selected_index + 1)
-                elif event.key == pygame.K_RETURN:
-                    selected_video = video_list[selected_index]
-                elif event.key == pygame.K_SPACE:
-                    selected_video = video_list[selected_index]
-                elif event.key == pygame.K_ESCAPE:
-                    pygame.quit()   
-    
-
-        screen.fill((0, 0, 0))
-
-        # Display video list
-        for i, video_name in enumerate(video_list):
-            text = font.render(video_name, True, (255, 255, 255))
-            text_rect = text.get_rect()
-            text_rect.center = (screen_width // 2, 100 + i * 50)
-            screen.blit(text, text_rect)
-
-        # Highlight the selected video
-        pygame.draw.rect(screen, (0, 255, 0), (250, 90 + selected_index * 50, 300, 50), 2)
+        screen.fill(BLACK)
+        for i, video_file in enumerate(video_files):
+            text = font.render(video_file, True, RED if i == selected_index else WHITE)
+            screen.blit(text, (50, 50 + i * 40))
 
         pygame.display.flip()
 
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                running = False
+            elif event.type == KEYDOWN:
+                if event.key == K_DOWN:
+                    selected_index = (selected_index + 1) % len(video_files)
+                elif event.key == K_UP:
+                    selected_index = (selected_index - 1) % len(video_files)
+                elif event.key == K_RETURN:
+                    selected_video = os.path.join(video_directory, video_files[selected_index])
+                    play_video(selected_video)
+                elif event.key == K_ESCAPE:
+                    pygame.quit()
+
+    # Clean up when done
+    if vlc_player is not None:
+        vlc_player.terminate()
+
     pygame.quit()
-
-# Function to play the selected video using VLC
-def play_video(video_file):
-    try:
-        #vlc_command = f"cvlc '{folder_path}{video_file}' --no-repeat --play-and-exit --fullscreen --no-xlib"
-        vlc_command = f"sudo -u pi cvlc '{folder_path}{video_file}' --no-repeat --play-and-exit --fullscreen"  # Replace with the appropriate VLC command
-        subprocess.Popen(vlc_command, shell=True)
-    except Exception as e:
-        print(f"Error running VLC: {e}")
-
-# Keyboard event listener thread
-def keyboard_listener():
-    global selected_video, selected_index
-
-    #time.sleep(10)
-    #selected_index = min(len(video_files) - 1, selected_index + 1)
-
-if __name__ == "__main__":
-    video_dir = folder_path
-    video_files = [f for f in os.listdir(video_dir) if f.endswith(".mp4")]
-
-    keyboard_thread = threading.Thread(target=keyboard_listener)
-    keyboard_thread.daemon = True  # Make the thread a daemon so it terminates when the main program ends
-    keyboard_thread.start()
-
-    while True:
-        selected_video = None
-        display_menu(video_files)
-
-        if selected_video:
-            video_path = os.path.join(video_dir, selected_video)
-            play_video(video_path)
