49386f03a9ff8b9208fce8744c0809a21efc1757
Thomas Williams <noboneslikejelly@gmail.com>
2023-11-10 11:50:25 +1300

    Added pygame display test

---
+++ TestPyGame.py
@@ -0,0 +1,28 @@
+import os
+import pygame
+import time
+
+def FindDisplayDriver():
+  for driver in ["fbcon", "directfb", "svgalib"]:
+    if not os.getenv("SDL_VIDEODRIVER"):
+      os.putenv("SDL_VIDEODRIVER", driver)
+    try:
+      pygame.display.init()
+      return True
+    except pygame.error:
+      pass
+  return False
+
+if __name__ == "__main__":
+  pygame.init()
+  if not FindDisplayDriver():
+    print("Failed to initialise display driver")
+  else:
+    pygame.mouse.set_visible(False)
+    width  = pygame.display.Info().current_w
+    height = pygame.display.Info().current_h
+    screen = pygame.display.set_mode((width, height), pygame.FULLSCREEN)
+    screen.fill((0x3F, 0, 0))
+    pygame.display.update()
+    time.sleep(30)
+    pygame.quit()
